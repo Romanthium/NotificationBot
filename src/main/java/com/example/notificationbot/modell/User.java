8320445c0fef2cdd8d9c7aae0af2c489813aebf6
romanthium <romanthium@gmail.com>
2023-06-18 18:25:04 +0200

    added @Email to User

--- src/main/java/com/example/notificationbot/modell/User.java
+++ src/main/java/com/example/notificationbot/modell/User.java
@@ -1,6 +1,7 @@
 package com.example.notificationbot.modell;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -24,6 +25,7 @@ public class User {
 
     @NotEmpty
     @Column(unique = true)
+    @Email
     private String login;
 
     @NotEmpty
