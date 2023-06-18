3f36f38dd755fa9eefa498eb9055df66761c081f
romanthium <romanthium@gmail.com>
2023-06-18 18:13:33 +0200

    added serice

---
+++ src/main/java/com/example/notificationbot/service/TaskService.java
@@ -0,0 +1,42 @@
+package com.example.notificationbot.service;
+
+
+import com.example.notificationbot.modell.Task;
+import com.example.notificationbot.repository.TaskRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Service
+@Transactional
+public class TaskService {
+
+    private final TaskRepository taskRepository;
+
+    @Transactional(readOnly = true)
+    public List<Task> findAll() {
+        return taskRepository.findAll();
+    }
+
+    @Transactional(readOnly = true)
+    public Optional<Task> findById(Long id) {
+        return taskRepository.findById(id);
+    }
+
+    public void save(Task task) {
+        taskRepository.save(task);
+    }
+
+    public void update(Long id, Task task) {
+        task.setId(id);
+        taskRepository.save(task);
+    }
+
+    public void delete(Long id) {
+        taskRepository.deleteById(id);
+    }
+}
---
+++ src/main/java/com/example/notificationbot/service/UserService.java
@@ -0,0 +1,47 @@
+package com.example.notificationbot.service;
+
+
+import com.example.notificationbot.modell.User;
+import com.example.notificationbot.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Service
+@Transactional
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Transactional(readOnly = true)
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    @Transactional(readOnly = true)
+    public Optional<User> findById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    @Transactional(readOnly = true)
+    public Optional<User> findByName(String login) {
+        return userRepository.findByLogin(login);
+    }
+
+    public void save(User user) {
+        userRepository.save(user);
+    }
+
+    public void update(Long id, User user) {
+        user.setId(id);
+        userRepository.save(user);
+    }
+
+    public void delete(Long id) {
+        userRepository.deleteById(id);
+    }
+}
