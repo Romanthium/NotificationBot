34d498da8b465b9e1da86d398dd6651f9ee5c664
romanthium <romanthium@gmail.com>
2023-06-21 03:15:27 +0200

    added search repository

--- src/main/java/com/example/notificationbot/repository/TaskRepository.java
+++ src/main/java/com/example/notificationbot/repository/TaskRepository.java
@@ -1,9 +1,19 @@
 package com.example.notificationbot.repository;
 
 import com.example.notificationbot.model.Task;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TaskRepository extends JpaRepository<Task, Long> {
+public interface TaskRepository extends JpaRepository<Task, Long>, JpaSpecificationExecutor<Task> {
+
+    static Specification<Task> topicContains(String keywordTopic) {
+        return (task, cq, cb) -> cb.like(cb.lower(task.get("topic")), "%" + keywordTopic + "%");
+    }
+
+    static Specification<Task> textContains(String keywordText) {
+        return (task, cq, cb) -> cb.like(cb.lower(task.get("text")), "%" + keywordText + "%");
+    }
 }
