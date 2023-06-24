bc2938f02216d0e854e4ef2f30d1670b74821ede
romanthium <romanthium@gmail.com>
2023-06-24 21:58:02 +0200

    updated repository task

--- src/main/java/com/example/notificationbot/repository/TaskRepository.java
+++ src/main/java/com/example/notificationbot/repository/TaskRepository.java
@@ -6,6 +6,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long>, JpaSpecificationExecutor<Task> {
 
@@ -16,4 +18,6 @@ public interface TaskRepository extends JpaRepository<Task, Long>, JpaSpecificat
     static Specification<Task> textContains(String keywordText) {
         return (task, cq, cb) -> cb.like(cb.lower(task.get("text")), "%" + keywordText.toLowerCase() + "%");
     }
+
+    List<Task> findAllByUserId(Long userId);
 }
